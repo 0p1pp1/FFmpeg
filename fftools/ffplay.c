924e6ce1dcd38d5a15f3bb081916f4f535db090b
0p1pp1 <0p1pp1[fms.freenet>
2015-07-02 20:25:21 +0900

    add DVB protocol support

--- fftools/ffplay.c
+++ fftools/ffplay.c
@@ -2715,6 +2715,7 @@ static int is_realtime(AVFormatContext *s)
 
     if(s->pb && (   !strncmp(s->url, "rtp:", 4)
                  || !strncmp(s->url, "udp:", 4)
+                 || !strncmp(s->url, "dvb:", 4)
                 )
     )
         return 1;
