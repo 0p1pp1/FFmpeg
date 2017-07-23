7bccafbd5f3f48ff970c0b41ec2a52f5b3bb5d60
0p1pp1 <0p1pp1@fms.freenet>
2017-07-23 22:55:24 +0900

    mpegts: fix equivalence check of EITs
    EIT is divided into sub-tables by service id, each with its own version/CRC.
    skip_check() does not support sub tables,
    so EIT info was updated by every sub table even with no updates.

--- libavformat/mpegts.c
+++ libavformat/mpegts.c
@@ -113,6 +113,8 @@ struct Program {
 
     /** have we found pmt for this program */
     int pmt_found;
+
+    int cur_eit_ver;
 };
 
 struct MpegTSContext {
@@ -303,6 +305,7 @@ static void clear_program(MpegTSContext *ts, unsigned int programid)
         if (ts->prg[i].id == programid) {
             ts->prg[i].nb_pids = 0;
             ts->prg[i].pmt_found = 0;
+            ts->prg[i].cur_eit_ver = -1;
         }
 }
 
@@ -323,6 +326,7 @@ static void add_pat_entry(MpegTSContext *ts, unsigned int programid)
     p->id = programid;
     p->nb_pids = 0;
     p->pmt_found = 0;
+    p->cur_eit_ver = -1;
     ts->nb_prg++;
 }
 
@@ -2523,11 +2527,7 @@ static void eit_cb(MpegTSFilter *filter, const uint8_t *section, int section_len
         return;
     if (h->tid != EIT_TID || h->sec_num != 0)
         return;
-    if (!h->cur_nxt)
-        return;
     if (ts->skip_changes)
-        return;
-    if (skip_identical(h, &filter->u.section_filter))
         return;
 
     program = NULL;
@@ -2537,7 +2537,14 @@ static void eit_cb(MpegTSFilter *filter, const uint8_t *section, int section_len
     if (!program || program->nb_stream_indexes <= 0)
         return;
 
-    filter->u.section_filter.version = h->version;
+    {
+        struct Program *p = get_program(ts, h->id);
+
+        if (!p || p->cur_eit_ver == h->version)
+            return;
+        p->cur_eit_ver = h->version;
+    }
+
     av_log(ts->stream, AV_LOG_DEBUG, "EIT:\n");
     hex_dump_debug(ts->stream, section, section_len);
 
